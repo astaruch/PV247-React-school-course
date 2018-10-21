11ca547c9675ad04392b9852becf15a513987f5c
njuro <juraj.noge@gmail.com>
2018-10-21 22:56:44 +0200

    Basic login component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import {MessageApp} from './components/MessageApp';
 
 export class App extends React.PureComponent {
     render(): JSX.Element {
-        const nick = 'Pussy Reaper 123';
         const message = 'New message!';
         const messages = [ 'Old message1', 'Old message2'];
         return (
-            <MessageApp nick={nick} message={message} messages={messages}/>
+            <MessageApp message={message} messages={messages}/>
         );
     }
 }
--- src/components/MessageApp.tsx
+++ src/components/MessageApp.tsx
@@ -3,14 +3,20 @@ import * as PropTypes from 'prop-types';
 
 import {ChannelList} from './ChannelList';
 import {IChannelItem} from './ChannelItem';
+import {Login} from './Login';
 
-interface IMessageApp {
-    nick: string;
+interface IMessageAppProps {
     message: string;
     messages: any[];
 }
 
-export class MessageApp extends React.PureComponent<IMessageApp> {
+interface IMessageAppState {
+    logged: boolean;
+    nick: string;
+}
+
+export class MessageApp extends React.PureComponent<IMessageAppProps, IMessageAppState> {
+
     static propTypes = {
         channels: PropTypes.arrayOf(PropTypes.shape({
             name: PropTypes.string.isRequired,
@@ -18,21 +24,42 @@ export class MessageApp extends React.PureComponent<IMessageApp> {
         })),
     };
 
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            logged: false,
+            nick: ''
+        };
+    }
+
+    onLogin = (loggedNick: string) => {
+        this.setState(() => ({
+            logged: true,
+            nick: loggedNick
+        }));
+    };
+
     private getChannels(): IChannelItem[] {
-        const channel1: IChannelItem = { name: 'General', numberOfNewMessages: 10 };
-        const channel2: IChannelItem = { name: 'Back office', numberOfNewMessages: 0};
-        const channel3: IChannelItem = { name: 'Spam', numberOfNewMessages: 1337 };
+        const channel1: IChannelItem = {name: 'General', numberOfNewMessages: 10};
+        const channel2: IChannelItem = {name: 'Back office', numberOfNewMessages: 0};
+        const channel3: IChannelItem = {name: 'Spam', numberOfNewMessages: 1337};
         return [channel1, channel2, channel3];
     }
 
     public render(): JSX.Element {
         const channels = this.getChannels();
 
+        if (this.state.logged) {
+            return (
+                <div className="MessageApp">
+                    Message App. Logged in as {this.state.nick}
+                    <ChannelList channels={channels}/>
+                </div>
+            );
+        }
+
         return (
-            <div className="MessageApp">
-                Message App
-                <ChannelList channels={channels}/>
-            </div>
+            <Login onLogin={this.onLogin}/>
         );
     }
 }
