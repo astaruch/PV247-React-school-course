189ddb103c014d175ae7385683e30a564eaa7d6c
njuro <juraj.noge@gmail.com>
2018-10-23 11:10:29 +0200

    Login restyling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -61,62 +61,30 @@ export class Login extends React.PureComponent<ILoginProps, ILoginState> {
             this.props.onLogin(this.state.nick);
             this.setResult(<Redirect to="/"/>);
         } else {
-            this.setResult(<h3>Incorrect username and/or password!</h3>);
+            this.setResult(<h4>Incorrect username and/or password!</h4>);
         }
     };
 
     render(): JSX.Element {
         return (
-            <div className="login row">
-                <div className="col-md-6 col-md-offset-3">
-                    <div className="panel panel-login">
-                        <div className="panel-heading">
-                            <div className="row">
-                                <div className="col-xs-6">
-                                    <a href="#" className="active" id="login-form-link">Login</a>
-                                </div>
-                                <div className="col-xs-6">
-                                    <a href="#" id="register-form-link">Register</a>
-                                </div>
-                            </div>
-                            <hr/>
-                        </div>
-                        <div className="panel-body">
-                            <div className="col-lg-12">
-                                <form id="login-form" onSubmit={this.handleSubmit}>
-                                    <div className="form-group">
-                                        <input type="text" onChange={this.setUsername} value={this.state.nick}/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input type="password" onChange={this.setPassword} value={this.state.password}/>
-                                    </div>
-                                    <div className="form-group text-center">
-                                        <input type="checkbox" className="" name="remember" id="remember"/>
-                                        <label htmlFor="remember">Remember me</label>
-                                    </div>
-                                    <div className="form-group">
-                                        <div className="row">
-                                            <div className="col-sm-6 col-sm-offset-3">
-                                                <input type="submit" name="login-submit" id="login-submit" className="form-control btn btn-login" value="Login"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="form-group">
-                                        <div className="row">
-                                            <div className="col-lg-12">
-                                                <div className="text-center">
-                                                    <a href="#" className="forgot-password">Forgot Password?</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                            </div>
-                                </form>
-                                <div className="row">
-                                    Hint: Username = admin, Password = pass
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="container">
+                <div className="wrapper">
+                    <form action="" method="post" name="Login_Form" className="form-signin"
+                          onSubmit={this.handleSubmit}>
+                        <h3 className="form-signin-heading">Sign In</h3>
+                        <hr className="colorgraph"/>
+                        <br/>
+
+                        <input type="text" className="form-control" name="Username" placeholder="Username"
+                               value={this.state.nick} onChange={this.setUsername} required autoFocus/>
+                        <input type="password" className="form-control" name="Password" placeholder="Password"
+                               value={this.state.password} onChange={this.setPassword} required/>
+                        <div className="hint">(Username = admin, Password = pass)</div>
+                        <button className="btn btn-lg btn-primary btn-block" name="Submit" value="Login"
+                                type="Submit">Login
+                        </button>
+                        {this.state.result}
+                    </form>
                 </div>
             </div>
         );
