fef9ccce5478a65b25c0042f8de710f2c56202d4
Andrej Staruch <staruch.andrej@gmail.com>
2018-11-09 19:27:32 +0100

    Adding more axtions.

--- src/actions/actionCreators.ts
+++ src/actions/actionCreators.ts
@@ -1,8 +1,15 @@
-import {MESSAGE_APP_LOGIN_USER} from '../constants/actionTypes';
+import {MESSAGE_APP_LOGIN_USER, MESSAGE_APP_SELECT_CHANNEL} from '../constants/actionTypes';
 
 export const loginUser = (username: string): Action => ({
     type: MESSAGE_APP_LOGIN_USER,
     payload: {
         username,
+    }
+});
+
+export const selectChannel = (number: number): Action => ({
+    type: MESSAGE_APP_SELECT_CHANNEL,
+    payload: {
+        number,
     }
 });
