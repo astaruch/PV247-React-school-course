68572303e0442f07dfdbf867990d2ae3d7e27a1d
Andrej Staruch <staruch.andrej@gmail.com>
2019-01-28 20:05:53 +0100

    tweaks to css

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -1,25 +1,28 @@
 import * as React from 'react';
 import * as Immutable from 'immutable';
 import {ChannelItemContainer} from '../containers/ChannelItemContainer';
-import {Button, Header, Menu, Segment} from 'semantic-ui-react';
+import {Button, Header, Icon, Menu, Segment} from 'semantic-ui-react';
 
 export interface IChannelListStateProps {
-    readonly channelIds: Immutable.List<Uuid>;
+  readonly channelIds: Immutable.List<Uuid>;
 }
 
 export class ChannelList extends React.PureComponent<IChannelListStateProps> {
-    public render(): JSX.Element {
-        return (
-            <Menu vertical className={'channel-list'}>
-                <Header as={'h2'} attached={'top'}>
-                    Channels  <Button icon={'add'} />
-                </Header>
-                <Segment attached>
-                    {this.props.channelIds && this.props.channelIds.map((channelId) => (
-                        <ChannelItemContainer id={channelId} key={channelId}/>
-                    ))}
-                </Segment>
-            </Menu>
-        );
-    }
+
+  render(): JSX.Element {
+    return (
+      <Menu vertical className={'channel-list'}>
+        <Header as={'h3'} className={'channel-list-header-text'}>
+          <Icon name={'unordered list'}/>
+          <Header.Content>Channels</Header.Content>
+          <Button className={'channel-list-header-button'} icon={'add'}/>
+        </Header>
+        <Segment attached>
+          {this.props.channelIds && this.props.channelIds.map((channelId) => (
+            <ChannelItemContainer id={channelId} key={channelId}/>
+          ))}
+        </Segment>
+      </Menu>
+    );
+  }
 }
