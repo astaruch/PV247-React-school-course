6bd5cfdb48f91d1cade9fd9f2b4e8a3c64f30970
Andrej Staruch <staruch.andrej@gmail.com>
2019-02-02 10:14:55 +0100

    Updating sended message in message list

--- src/reducers/messagesReducer.ts
+++ src/reducers/messagesReducer.ts
@@ -2,11 +2,17 @@ import * as Immutable from 'immutable';
 import {IMessage} from '../models/IMessage';
 import {combineReducers} from 'redux';
 import {MESSAGES_RETRIEVING_ENDED} from '../actions/globalActions';
+import {MESSAGE_SENDING_ENDED} from '../actions/messageActions';
 
 const asMap = (prevState = Immutable.Map<Uuid, IMessage>(), action: Action): Immutable.Map<Uuid, IMessage> => {
   switch (action.type) {
     case MESSAGES_RETRIEVING_ENDED:
       return Immutable.Map(action.payload.messages.map((message: IMessage) => [message.id, message]));
+
+    case MESSAGE_SENDING_ENDED:
+      const message = action.payload.message;
+      return prevState.set(message.id, message);
+
     default:
       return prevState;
   }
@@ -16,6 +22,11 @@ const asList = (prevState = Immutable.List<Uuid>(), action: Action): Immutable.L
   switch (action.type) {
     case MESSAGES_RETRIEVING_ENDED:
       return Immutable.List(action.payload.messages.map((message: IMessage) => message.id));
+
+    case MESSAGE_SENDING_ENDED:
+      const message = action.payload.message;
+      return prevState.push(message.id);
+
     default:
       return prevState;
   }
